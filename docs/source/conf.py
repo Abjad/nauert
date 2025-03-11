5b6aad6edb72a9d72893bd32d7405c539c497e27
Trevor Bača <trevor.baca@gmail.com>
2025-03-11 14:32:09 -0400

    Clean up conf.py (#101)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath("../../source"))
 
 autodoc_member_order = "groupwise"
 
-copyright = "2016-2025, Joséphine Wolf Oberholtzer & Tsz Kiu Pang"
+copyright = "2016-2025, Joséphine Wolf Oberholtzer & Tsz Kiu Pang."
 
 extensions = [
     "abjad.ext.sphinx",
@@ -29,18 +29,12 @@ extensions = [
 graphviz_dot_args = ["-s32"]
 graphviz_output_format = "svg"
 
-# html_favicon = "_static/favicon.ico"
-# html_logo = "_static/abjad-logo.png"
-# html_show_copyright = False
-# html_show_sourcelink = False
-# html_show_sphinx = False
+html_last_updated_fmt = "%b %d, %Y"
+html_show_sphinx = False
 html_static_path = ["_static"]
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
     "canonical_url": "https://abjad.github.io/nauert",
-    # most important setting:
-    # navigation_depth=1 makes sidebar completely flat;
-    # leave flat navigation in place forever:
     "navigation_depth": 1,
     "sticky_navigation": False,
     "style_external_links": True,
@@ -52,25 +46,22 @@ intersphinx_mapping = {
     "sphinx": ("http://www.sphinx-doc.org/en/master/", None),
 }
 
-project = "nauert"
+project = "Nauert"
 pygments_style = "sphinx"
-
-release = nauert.__version__
 
 smartquotes = True
 
 templates_path = ["_templates"]
 todo_include_todos = True
 
-uqbar_api_title = "Nauert API"
-uqbar_api_source_paths = ["nauert"]
-uqbar_api_root_documenter_class = "uqbar.apis.SummarizingRootDocumenter"
-uqbar_api_module_documenter_class = "uqbar.apis.SummarizingModuleDocumenter"
 uqbar_api_member_documenter_classes = [
     "uqbar.apis.FunctionDocumenter",
     "uqbar.apis.SummarizingClassDocumenter",
 ]
-
+uqbar_api_module_documenter_class = "uqbar.apis.SummarizingModuleDocumenter"
+uqbar_api_root_documenter_class = "uqbar.apis.SummarizingRootDocumenter"
+uqbar_api_source_paths = ["nauert"]
+uqbar_api_title = "Nauert API"
 uqbar_book_console_setup = [
     "import abjad",
     "import nauert",
