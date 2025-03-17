72efd4b0358ea453889b060a51ff4d1f868e0086
Trevor Bača <trevor.baca@gmail.com>
2025-03-16 20:46:34 -0400

    Clean up conf.py (#107)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ html_theme_options = {
     "navigation_depth": 1,
     "sticky_navigation": False,
     "style_external_links": True,
-    "style_nav_header_background": "#ffeedd",
+    "style_nav_header_background": "#000000",
 }
 
 intersphinx_mapping = {
