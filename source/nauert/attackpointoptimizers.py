ddee077a745a3a6e75adf59bb3f5a19c033aaa90
Trevor Bača <trevor.baca@gmail.com>
2025-09-26 11:17:33 -0400

    Remove abjad.LogicalTie inits

--- source/nauert/attackpointoptimizers.py
+++ source/nauert/attackpointoptimizers.py
@@ -357,14 +357,10 @@ class NaiveAttackPointOptimizer(AttackPointOptimizer):
                 tempos = leaf._get_indicators(abjad.MetronomeMark)
                 if tempos:
                     if current_sub_logical_tie:
-                        current_sub_logical_tie = abjad.LogicalTie(
-                            current_sub_logical_tie
-                        )
                         sub_logical_ties.append(current_sub_logical_tie)
                     current_sub_logical_tie = []
                 current_sub_logical_tie.append(leaf)
             if current_sub_logical_tie:
-                current_sub_logical_tie = abjad.LogicalTie(current_sub_logical_tie)
                 sub_logical_ties.append(current_sub_logical_tie)
             for sub_logical_tie in sub_logical_ties:
                 abjad.mutate._fuse_leaves_by_immediate_parent(sub_logical_tie)
